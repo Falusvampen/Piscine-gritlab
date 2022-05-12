642cad1109fdc18fbf6764991718a90b32b3f4bc
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2022-05-12 05:01:51 +0300

    My Grit:lab repository

---
+++ piscine-go/Learning PowerShell/who-are-you.sh
@@ -0,0 +1,1 @@
+curl https://01.gritlab.ax/assets/superhero/all.json | jq '.[]| select( .id ==70) |.name'
---
+++ piscine-go/pointone.go
@@ -0,0 +1,12 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PointOne(n *int){
+	n := 0
+
+	var pointer *int = &n
+
+
+
+}
